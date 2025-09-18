7427658f265b87e43b2b5b2a3c6d45b5d491e5fa
Austin Namuye <austinnamuye@gmail.com>
2025-09-18 23:06:41 +0000

    create umoja dashboard  let it have sauce swap pool TAAS like earlier us

--- src/app/umoja/page.tsx
+++ src/app/umoja/page.tsx
@@ -2,8 +2,6 @@ import { AppHeader } from '@/components/app-header';
 import { UmojaStats } from '@/components/umoja/umoja-stats';
 import { SwapPools } from '@/components/umoja/swap-pools';
 import { UcseMarketplace } from '@/components/umoja/ucse-marketplace';
-import { UmojaChart } from '@/components/umoja/umoja-chart';
-
 
 export default function UmojaPage() {
   return (
@@ -15,14 +13,9 @@ export default function UmojaPage() {
 
       <UmojaStats />
       
-      <div className="grid grid-cols-1 lg:grid-cols-5 gap-6">
-        <div className="lg:col-span-3 flex flex-col gap-6">
-            <UmojaChart />
-            <SwapPools />
-        </div>
-        <div className="lg:col-span-2">
-            <UcseMarketplace />
-        </div>
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+        <SwapPools />
+        <UcseMarketplace />
       </div>
     </div>
   );
